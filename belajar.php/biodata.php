1c4ecc822f6ef1e507c19c2312802ea006dabc3b
noviari38 <noviaashari67@gmail.com>
2026-01-21 09:45:59 +0700

    upbidata

---
+++ belajar.php/biodata.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Nama: Novia Ashari Nur Rohman <br>";
+    echo "Kelas: XI TJKT 2 <br>";
+    echo "No absen: 25 <br>";
+    echo "Ekstrakulikuler: Student English Fun (SEF) <br>";
+    echo "Sekolah: SMKN 2 Baleendah <br>";
+?>
