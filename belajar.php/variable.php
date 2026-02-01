aee19a7cd85b18a1db7913d8f62c7911b2305b53
noviari38 <noviaashari67@gmail.com>
2026-02-01 16:59:05 +0700

    upvrbl

---
+++ belajar.php/variable.php
@@ -0,0 +1,13 @@
+<?php
+    $nama_lengkap = "Novia Ashari Nur Rohman";
+    $jenis-kelamin = 'P';
+    $umur = 17;
+    $berat = 53;
+    $tinggi = 154;
+
+    echo "Nama : $nama_lengkap<br>";
+    echo "Jenis Kelamin : $jenis_kelamin<br>";
+    echo "Umur : $umur<br>";
+    echo "Berat Badan : $berat<br>";
+    echo "Tinggi Badan : $tinggi<br>";
+?>
