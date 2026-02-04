ebe04236013912490d2209c1d42021ec716610db
noviari38 <noviaashari67@gmail.com>
2026-02-04 09:00:37 +0700

    upvrb

--- belajar.php/variable.php
+++ belajar.php/variable.php
@@ -1,13 +1,23 @@
 <?php
+    // tipe data char (karakter)
+    $jenis_kelamin = 'P';
+
+    // tipe data string (teks)
     $nama_lengkap = "Novia Ashari Nur Rohman";
-    $jenis-kelamin = 'P';
+
+    // tipe data integer
     $umur = 17;
-    $berat = 53;
+
+    // tipe data float
+    $berat = 55;
+
+    // tipe data float
     $tinggi = 154;
 
-    echo "Nama : $nama_lengkap<br>";
-    echo "Jenis Kelamin : $jenis_kelamin<br>";
-    echo "Umur : $umur<br>";
-    echo "Berat Badan : $berat<br>";
-    echo "Tinggi Badan : $tinggi<br>";
+    echo "Nama: $nama_lengkap<br>";
+    echo "Jenis Kelamin: $jenis_kelamin<br>";
+    echo "Umur: $umur tahun<br>";
+    echo "berat badan: $berat kg<br>";
+    echo "tinggi badan: $tinggi cm<br>";
+    echo "menikah: $menikah";
 ?>
